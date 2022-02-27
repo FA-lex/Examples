f9298da1598743b176c83ab2fbe1b7e2b88c6e37
FA-lex <a.good.lex@gmail.com>
2022-02-27 22:00:28 +0800

    add Example 004 Div

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,9 @@
+﻿// Целочисленное деление (без дробной части)
+int numberA = 12;
+int numberB = 5;
+Console.WriteLine(numberA/numberB);
+
+// Дробное деление
+double numberC = 12;
+double numberD = 5;
+Console.WriteLine(numberC/numberD);
