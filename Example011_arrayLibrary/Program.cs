de7bb9e1fb31ee51f9b82f697412d0cf9757908f
FA-lex <a.good.lex@gmail.com>
2022-03-03 22:27:38 +0800

    add Example011 array Library from lecture 2

---
+++ Example011_arrayLibrary/Program.cs
@@ -0,0 +1,54 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection [index] = new Random().Next(0, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    // int position = 0; при такой записи если элемента нет, в выводе будет 0 (первый элемент), 
+    //                   но это скорей всего будет неправда, поэтому принято указывать "-1", 
+    //                   что будет обозначать "индекс не найден"
+    int position = -1;
+
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+array[4] = 4;
+array[6] = 4;
+
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
