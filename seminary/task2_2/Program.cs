838115c3d1b01780dc11b2219ae7f9069c46f0e4
FA-lex <a.good.lex@gmail.com>
2022-03-04 20:43:21 +0800

    add task2_2 into seminary

---
+++ seminary/task2_2/Program.cs
@@ -0,0 +1,39 @@
+﻿// task2_2: Определить номер четверти плоскости, 
+// в которой находится точка с координатами Х и У, 
+// причем X ≠ 0 и Y ≠ 0
+
+Console.WriteLine("Для определения номера четверти плоскости, в которой находится точка введите координатаы Х и У ");
+Console.Write("X: ");
+int koordX = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Y: ");
+int koordY = Convert.ToInt32(Console.ReadLine());
+
+if (koordX > 0 && koordY > 0)
+{
+    Console.WriteLine("точка принадлежит 1-й координатой четверти");
+}
+else if (koordX < 0 && koordY > 0)
+{
+    Console.WriteLine("точка принадлежит 2-й координатой четверти");
+}
+else if (koordX < 0 && koordY < 0)
+{
+    Console.WriteLine("точка принадлежит 3-й координатой четверти");
+}
+else if (koordX > 0 && koordY < 0)
+{
+    Console.WriteLine("точка принадлежит 4-й координатой четверти");
+}
+else if (koordX == 0 && koordY == 0)
+{
+    Console.WriteLine("точка непринадлежит какой-либо четверти, она находится в центре координатной плоскости.");
+}
+else if (koordX == 0 || koordY == 0)
+{
+    Console.WriteLine("точка непринадлежит какой-либо четверти, она лежит на кординатной оси");
+}
+else
+{
+    Console.WriteLine("!!!не предвиденная ситуация!!!");
+}
