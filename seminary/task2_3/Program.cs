5bf31b5e12d207870610d6ca7d4fe7ad08e84ee9
FA-lex <a.good.lex@gmail.com>
2022-03-04 20:56:17 +0800

    add task2_3 into seminary

---
+++ seminary/task2_3/Program.cs
@@ -0,0 +1,10 @@
+﻿// task2_3: Программа проверяет пятизначное число на палиндромом.
+
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number %10 == number / 10000 && (number %100)/10 == (number / 1000)%10)
+{
+    Console.WriteLine("данное число является полиндромом");
+}
+else {Console.WriteLine("данное число не является полиндромом");}
