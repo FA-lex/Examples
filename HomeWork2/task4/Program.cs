6f2a6d0e3d158c2233a43c38fcd6170cdd5a418b
FA-lex <a.good.lex@gmail.com>
2022-03-05 00:12:11 +0800

    add task4 in home work

---
+++ HomeWork2/task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+Console.WriteLine("Введите первое число: ");
+double firstNum = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+double secondNum = Convert.ToDouble(Console.ReadLine());
+
+if (secondNum * secondNum == firstNum)
+{
+    Console.WriteLine("Первое число является квадратом второго. ");
+}
+else if(firstNum * firstNum == secondNum)
+{
+    Console.WriteLine("Второе число является квадратом первого. ");
+}
+else {Console.WriteLine("Ни одно из чисел не является квадратом другого. ");}
