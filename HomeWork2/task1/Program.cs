abb1cf281ebb1b6f5e99f7951459049702d9a5f6
FA-lex <a.good.lex@gmail.com>
2022-03-04 23:16:55 +0800

    add task1 in home work

---
+++ HomeWork2/task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать числа от -N до N
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int count = - num;
+
+while (count <= num)
+{
+    Console.Write(count + ", ");
+    count++;
+}
+Console.WriteLine( );
