141c568e982d2ae223edd55be85c7919e22226dc
FA-lex <a.good.lex@gmail.com>
2022-03-04 23:26:24 +0800

    add task2 in home work

---
+++ HomeWork2/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать четные числа от 1 до N
+
+Console.WriteLine("Чтобы показать четные числа от 1 до N введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int count = 2;
+
+while (count <= N)
+{
+    Console.Write(count + ", ");
+    count += 2;
+}
+Console.WriteLine( );
