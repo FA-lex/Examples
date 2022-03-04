ef04f065190505f0f55e6fa4a2377281e6586672
FA-lex <a.good.lex@gmail.com>
2022-03-04 20:06:00 +0800

    add task2_3 into seminary

---
+++ seminary/task2_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// task2_1: Дано число. Проверить кратно ли оно 7 и 23
+
+Console.Write("Введите число для проверки на кратность 7-и и 23-м: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 7 == 0 && number %23 == 0)
+{
+    Console.WriteLine("Число введено Вами (" + number + ") кратно 7 и 23");
+}
+else if(number % 7 == 0)
+{
+    Console.WriteLine("Число введено Вами (" + number + ") кратно только 7");
+}
+else if(number % 23 == 0)
+{
+    Console.WriteLine("Число введено Вами (" + number + ") кратно только 23");
+}
+else
+{
+    Console.WriteLine("Число введено Вами (" + number + ") некратно ни 7 ни 23");
+}
