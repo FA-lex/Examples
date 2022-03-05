ce4a071300c68fbbc7695b27c8cde4ec33140320
FA-lex <a.good.lex@gmail.com>
2022-03-06 01:04:51 +0800

    add task 3 into home work

---
+++ HomeWork2/task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+
+Console.Write("Введите номер дня недели: ");
+int numDay = Convert.ToInt16(Console.ReadLine());
+
+if(numDay == 1 || numDay == 2 || numDay == 3 || numDay == 4)
+{
+    Console.WriteLine("День рабочий.");
+}
+else if(numDay == 5)
+{
+    Console.WriteLine("Ну почти, Пятница-маленькая Суббота. Но все-же день рабочий.");
+}
+else if(numDay == 6)
+{
+    Console.WriteLine("Выходной, Суббота");
+}
+else if(numDay == 7)
+{
+    Console.WriteLine("Выходной, Воскресенье");
+}
+else
+{
+    Console.WriteLine("Такого номера дня недели нет.");
+}
