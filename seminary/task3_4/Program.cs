639a3479840396985ec502a29b39b604b1095a93
FA-lex <a.good.lex@gmail.com>
2022-03-06 18:23:07 +0800

    add task3_4 into seminary

---
+++ seminary/task3_4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задать массив из 8-и элементов и вывести на экран
+
+Console.WriteLine("введите восемь элементов через энтер");
+int[] nums = new int[8];
+for (int i = 0; i < 8; i++)
+{
+    nums[i] = int.Parse(Console.ReadLine());
+}
+
+Console.Write("\nВведенный массив : ");
+
+for (int i = 0; i < 8; i++)
+{
+    Console.Write(nums[i] + " ");
+} 
+Console.WriteLine(" ");
