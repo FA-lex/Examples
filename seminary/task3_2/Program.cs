8b77f02fd18df070d34cd3490b7ab314a4ba3c61
FA-lex <a.good.lex@gmail.com>
2022-03-06 19:24:41 +0800

    add task3_2 into seminary

---
+++ seminary/task3_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+Console.Write("Введите число для возведения в степень: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите в какую степень возвдить: ");
+int b = int.Parse(Console.ReadLine());
+
+int c = 1;
+int count =1;
+while (count <= b)
+{
+    c = c * a;
+    count++;
+}
+
+Console.WriteLine(a + " в степени " + b + " равно " + c);
