2e24f261f17d67da60dfef5af9116629f0d24741
FA-lex <a.good.lex@gmail.com>
2022-03-06 18:22:06 +0800

    add task3_3 into seminary

---
+++ seminary/task3_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Определить количество цифр в числе
+
+Console.WriteLine("введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("В числе, которое Вы ввели (" + num + ") ");
+int i = 0;
+while (num != 0)
+{
+    num = num / 10;
+    i++; 
+}
+Console.WriteLine(i + " цифр.");
